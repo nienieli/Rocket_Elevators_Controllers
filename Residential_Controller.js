777e24942ea2b460a3dccaa1792047117780d153
nienieli <yannickbellefleur@outlook.fr>
2019-06-17 21:56:03 -0400

    1/2 js

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -38,7 +38,7 @@ function init() {
     class Elevator {
         constructor() {
             this.floorNumber = floorNumber;
-            this.statut = "idle";
+            this.status = "idle";
             this.direction;
             this.doorStatus = "closed";
             this.positionNow = 1;
@@ -48,7 +48,7 @@ function init() {
             this.originPosition2 = 1;
             this.floorRequestButton = {
                 this.light = off;
-                floor;
+                this.floor;
             }
         }
     }
@@ -61,13 +61,13 @@ function moveToOrigin1(elevator) {
     while (elevator.positionNow = elevator.originPosition1)
         do {
             if (elevator.positionNow > elevator.originPosition1) {
-                elevator.floor -= 1 ;
+                elevator.positionNow -= 1;
                 elevator.status = "moveDown";
             } else {
-                elevator.floor += 1 ;
+                elevator.positionNow += 1;
                 elevator.status = "moveUP";
             }
-            elevator.status = "stop"                        //pas certain que cest la que ca va
+            elevator.status = "stop" //pas certain que cest la que ca va
         }
 };
 
@@ -75,18 +75,86 @@ function moveToOrigin2(elevator) {
     while (elevator.positionNow = elevator.originPosition2)
         do {
             if (elevator.positionNow > elevator.originPosition1) {
-                elevator.floor -= 1 ;
+                elevator.positionNow -= 1;
                 elevator.status = "moveDown";
             } else {
-                elevator.floor += 1 ;
+                elevator.positionNow += 1;
                 elevator.status = "moveUP";
             }
-            elevator.status = "stop"
+            elevator.status = "stop";
         }
 };
 
-function move (elevator){
+function move(elevator) {
     if (elevator.requestList.floor[0] > elevator.positionNow) {
-        while 
+        while (elevator.requestList.floor[0] = elevator.positionNow)
+            do {
+                elevator.positionNow += 1;
+                elevator.status = "moveUp";
+            }
+        elevator.status = "stop"
+    } else if (elevator.requestList.floor[0] = elevator.positionNow) {
+        elevator.status = "stop";
+    } else {
+        while (elevator.requestList.floor[0] = elevator.positionNow)
+            do {
+                elevator.positionNow -= 1;
+                elevator.status = "moveDown"
+            }
+        elevator.status = "stop"
     }
+    return elevator.status
+}
+
+function sortRequestList(elevator) {
+    if (elevator.status = "moveUp") {
+        // A FAIRE 
+    } else if (elevator.status = "moveDown") {
+        //A FAIRE 
+    }
+}
+
+function elevatorStatus(elevator) {
+    if (elevator.requestList.floor[0] || elevator.requestList.userFloor[0] > elevator.positionNow) {
+        elevator.status = "moveUp"
+    } else if (elevator.requestList.floor[0] || elevator.requestList.userFloor = elevator.positionNow) { // A VERIFIER 
+        elevator.status = "stop"
+    } else {
+        elevator.status = "moveDown"
+    }
+}
+
+function findElevator(CallButton && floorRequestButton) {
+    best
+    case = 1;
+    selectedElevator = 1;
+    this.elevator = [];
+    for (let i = 0; i < this.elevator; i++) {
+        this.elevator.push(new Elevator);
+    }
+    //  closestElevator = ABSOLUTE POSITION OF ???
+    if (elevator.status = "idle") {
+        new bestCase = NumbersOfFloor + 1 - closestElevator
+    } else if (elevator.status = "moveDown") {
+        CallButton.userFloor > elevator.positionNow
+        new bestCase = 1
+    } else if (CallButton < elevator.positionNow && CallButton.direction = elevatorStatus) {
+        new bestCase = NumbersOfFloor + 2 - closestElevator
+    } else if (CallButton < elevator.positionNow && CallButton.direction = elevatorStatus) {
+        new bestCase = NumbersOfFloor + 1 - closestElevator
+    };
+    else if (elevator.status = "moveUp") {
+        if (CallButton.userFloor < elevator.positionNow) {
+            new bestCase = 1
+        } else if (CallButton.userFloor > elevator.positionNow && CallButton.direction = elevator.status) {
+            new bestCase = NumbersOfFloor + 2 - closestElevator
+        } else if (allButton.userFloor > elevator.positionNow && CallButton.direction = elevator.status) {
+            new bestCase = NumbersOfFloor + 1 - closestElevator
+        }
+    }
+    if (new bestCase > bestCase) {
+        selectedElevator = elevator;
+        bestCase = new bestCase;
+    }
+    return selectedElevator.requestList(CallButton.floor)
 }
